d4e8d5a65fe335055b16364e46ab5118066e678b
AlfredoJGZ <garciazambranoalfredo@gmail.com>
2020-09-17 23:51:05 -0400

    finalizada barra de navegacion superior, agregada img alerti-logo.png a la carpeta assets

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faBars, faSignOutAlt } from '@fortawesome/free-solid-svg-icons'
+
+function Navbar() {
+  return(
+    <View style={styles.navbar}>
+      <FontAwesomeIcon icon={ faBars } size={25} color={'white'} />
+      <Image 
+        style={styles.navbarImg}
+        source={require('./assets/alerti-logo.png')}  
+      />
+      <FontAwesomeIcon icon={ faSignOutAlt } size={25} color={'white'} />
+    </View>
+  )
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navbar />
   );
 }
 
@@ -18,4 +30,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  navbar: {
+    backgroundColor: '#1E272E',
+    marginTop: 25, 
+    height: 50,
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.27,
+    shadowRadius: 4.65,
+    elevation: 6,
+  },
+  navbarImg: {
+    width: 132,
+    height: 28
+  }
 });
