3e376d6985a9b49478ec5d6104401d6111167b97
AlfredoJGZ <garciazambranoalfredo@gmail.com>
2020-10-07 22:59:59 -0400

    instalada dependencia react-native-screens; primeras esctructuras de onboarding

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet } from 'react-native';
-import { createDrawerNavigator, DrawerItem } from '@react-navigation/drawer';
+import { enableScreens } from 'react-native-screens';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import DashboardsScreen from './components/DashboardsScreen';
 import OpcionesScreen from './components/OpcionesScreen';
@@ -13,27 +15,39 @@ import FalabellaChileScreen from './components/FalabellaChileScreen';
 import FalabellaColombiaScreen from './components/FalabellaColombiaScreen';
 import CmrMexicoScreen from './components/CmrMexicoScreen';
 import AmbientesScreen from './components/AmbientesScreen';
+import LoginScreen from './components/LoginScreen'; 
 
 const Drawer = createDrawerNavigator();
+enableScreens();
+const Stack = createNativeStackNavigator();
+
+
 
 export default function App() {
+  const logged = false; 
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName='Dashboards' drawerStyle={styles.drawer} drawerContentOptions={drawerItemsStyles}>
-        <Drawer.Screen name='Dashboards' component={DashboardsScreen} />
-        <Drawer.Screen name='Opciones' component={OpcionesScreen} />
-        <Drawer.Screen name='Monitores' component={MonitoresScreen} />
-        <Drawer.Screen name='Falabella Perú' component={FalabellaPeruScreen} options={{ drawerLabel: 'Home' }} />
-        <Drawer.Screen name='CMR Argentina' component={CmrArgentinaScreen} />
-        <Drawer.Screen name='CMR Chile' component={CmrChileScreen} />
-        <Drawer.Screen name='Falabella Chile' component={FalabellaChileScreen} />
-        <Drawer.Screen name='Falabella Colombia' component={FalabellaColombiaScreen} />
-        <Drawer.Screen name='CMR México' component={CmrMexicoScreen} />
-        <Drawer.Screen name='Ambientes Previos' component={AmbientesScreen} />     
-      </Drawer.Navigator>
+      {logged ? (
+        <Drawer.Navigator initialRouteName='Dashboards' drawerStyle={styles.drawer} drawerContentOptions={drawerItemsStyles}>
+          <Drawer.Screen name='Dashboards' component={DashboardsScreen} />
+          <Drawer.Screen name='Opciones' component={OpcionesScreen} />
+          <Drawer.Screen name='Monitores' component={MonitoresScreen} />
+          <Drawer.Screen name='Falabella Perú' component={FalabellaPeruScreen} options={{ drawerLabel: 'Home' }} />
+          <Drawer.Screen name='CMR Argentina' component={CmrArgentinaScreen} />
+          <Drawer.Screen name='CMR Chile' component={CmrChileScreen} />
+          <Drawer.Screen name='Falabella Chile' component={FalabellaChileScreen} />
+          <Drawer.Screen name='Falabella Colombia' component={FalabellaColombiaScreen} />
+          <Drawer.Screen name='CMR México' component={CmrMexicoScreen} />
+          <Drawer.Screen name='Ambientes Previos' component={AmbientesScreen} />     
+        </Drawer.Navigator>
+      ):(
+        <Stack.Navigator>
+          <Stack.Screen name="Login" component={LoginScreen} />
+        </Stack.Navigator>
+      )} 
     </NavigationContainer>
   );
-}
+};
 
 const styles = StyleSheet.create({
   drawer:{
---
+++ components/LoginScreen.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import Navbar from './Navbar'; 
+
+function LoginScreen() {
+    return ( 
+        <Text>PANTALLA DE LOGIN</Text>
+    );
+};
+
+export default LoginScreen; 
